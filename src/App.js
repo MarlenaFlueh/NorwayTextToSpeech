1bde1c3b870db85f91e92a8686d6b76c2011c72a
softcracker <m.flueh@protonmail.com>
2020-02-13 14:57:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './logo.png';
 import './App.css';
+import { Table } from 'reactstrap';
+import { list } from "./config"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    deuVisible: true,
+    norVisible: true
+  }
+
+  render() {
+
+    const checkWord = (word) => {
+      return word.split("").map(item => item === " " ? "%20" : item).join("")
+    }
+
+    const readText = (word) => {
+      var audio = new Audio();
+      audio.src = `https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=${checkWord(word)}&tl=no&total=1&idx=0&textlen=${word.length}`;
+      audio.play();
+    }
+
+    const visibleDeu = () => {
+      this.setState({ deuVisible: !this.state.deuVisible })
+    }
+
+    const visibleNor = () => {
+      this.setState({ norVisible: !this.state.norVisible })
+    }
+
+    const tableContent = () => {
+
+      return list.map(item => (
+        <tr>
+          <th className="padding-top" scope="row">{list.indexOf(item) + 1}</th>
+          {this.state.deuVisible ? <td className="padding-top">{item.deu}</td> : <td></td>}
+          {this.state.norVisible ? <td className="padding-top">{item.nor}</td> : <td></td>}
+          <button className="volumeButton" onClick={() => readText(item.speech)}>🔊</button>
+        </tr>
+      )
+      )
+    }
+
+    return (
+      <div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <Table bordered>
+              <thead>
+                <tr>
+                  <th></th>
+                  <th className="padding"><button className="button" onClick={() => visibleDeu()}>Deutsch</button></th>
+                  <th className="padding"><button className="button" onClick={() => visibleNor()}>Norwegisch</button></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableContent()}
+              </tbody>
+            </Table>
+          </header>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,101 @@
+const list = [
+    {
+        "deu": "Hi",
+        "nor": "Hei",
+        "speech": "Hei"
+    },
+    {
+        "deu": "Ich heiße",
+        "nor": "Jeg heter",
+        "speech": "jeg heter"
+    },
+    {
+        "deu": "Guten Morgen",
+        "nor": "God morgen",
+        "speech": "God morgen"
+    },
+    {
+        "deu": "Moin/Morgen",
+        "nor": "Morn",
+        "speech": "Morn"
+    },
+    {
+        "deu": "Danke für letztens.",
+        "nor": "Takk for sist.",
+        "speech": "Takk for sist"
+    },
+    {
+        "deu": "Danke gleichfalls (vorheriges)",
+        "nor": "Takk i like mate.",
+        "speech": "Takk i like mate."
+    },
+    {
+        "deu": "Danke dir.",
+        "nor": "Takk skal du ha.",
+        "speech": "Takk skal du ha."
+    },
+    {
+        "deu": "Vielen Dank!",
+        "nor": "Mange takk!",
+        "speech": "Mange takk."
+    },
+    {
+        "deu": "Tausend Dank!",
+        "nor": "Tusen takk!",
+        "speech": "Tusen takk"
+    },
+    {
+        "deu": "Gleichfalls.",
+        "nor": "Så ha du.",
+        "speech": "så ha du."
+    },
+    {
+        "deu": "Dafür nicht.",
+        "nor": "Ingenting å takke for.",
+        "speech": "ingenting å takke for."
+    },
+    {
+        "deu": "Guten Abend!",
+        "nor": "God kveld!",
+        "speech": "god kveld"
+    },
+    {
+        "deu": "Tschüss!",
+        "nor": "Ha det!",
+        "speech": "ha det"
+    },
+    {
+        "deu": "Mach’s gut!",
+        "nor": "Ha det bra!",
+        "speech": "Ha de bra."
+    },
+    {
+        "deu": "Ich heiße",
+        "nor": "Jeg heter",
+        "speech": "jeg heter"
+    },
+    {
+        "deu": "Ich wohne in Oslo.",
+        "nor": "Jeg bor i Oslo.",
+        "speech": "Jeg bor i Oslo."
+    },
+    {
+        "deu": "Ich komme aus Oslo.",
+        "nor": "Jeg kommer fra Oslo.",
+        "speech": "jeg kommer fra Oslo"
+    },
+    {
+        "deu": "Ich komme nicht aus Oslo.",
+        "nor": "Jeg kommer ikke fra Oslo.",
+        "speech": "jeg kommer ikke fra Oslo"
+    },
+    {
+        "deu": "Ich auch nicht.",
+        "nor": "Ikke jeg heller.",
+        "speech": "Ikke jeg heller."
+    }
+]
+
+export {
+    list
+}
