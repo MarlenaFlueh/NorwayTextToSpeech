7865f111c680faa57325479e66e59f60e6fe1206
softcracker <m.flueh@protonmail.com>
2020-02-14 14:18:01 +0100

    try out package

--- src/Music.js
+++ src/Music.js
@@ -1,32 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
+import AudioPlayer from 'react-h5-audio-player'
 
-class Music extends Component {
-    state = {
-        play: false
-    }
-    audio = new Audio(`https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=Katt&tl=no&total=1&idx=0&textlen=${4}`)
-
-    componentDidMount() {
-        this.audio.addEventListener('ended', () => this.setState({ play: false }));
-    }
-
-    componentWillUnmount() {
-        this.audio.removeEventListener('ended', () => this.setState({ play: false }));
-    }
-
-    togglePlay = () => {
-        this.setState({ play: !this.state.play }, () => {
-            this.state.play ? this.audio.play() : this.audio.pause();
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <button onClick={this.togglePlay}>{this.state.play ? 'Pause' : 'Play'}</button>
-            </div>
-        );
-    }
-}
+const Music = () => (
+    <AudioPlayer
+        src={`https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=Katt&tl=no&total=1&idx=0&textlen=${4}`}
+        onPlay={e => console.log("onPlay")}
+    // other props here
+    />
+)
 
 export default Music;
